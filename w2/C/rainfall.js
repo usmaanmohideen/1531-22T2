33a76ae46aa11acd3ca719c540779ec5da560bee
Usmaan Mohideen <68462538+usmaanmohideen@users.noreply.github.com>
2022-06-08 14:52:05 +1000

    Update rainfall.js

--- w2/C/rainfall.js
+++ w2/C/rainfall.js
@@ -4,5 +4,17 @@
  * Return null if there are no positive integers.
  */
 function rainfall(integers) {
-    // TODO
+  let total = 0
+  let count = 0
+  for (const integer in integers) {
+    if (integer > 0) {
+      total += integer
+      count++;
+    }
   }
+  if (count === 0) {
+    return null;
+  } else {
+    return total / count;
+  }
+}
